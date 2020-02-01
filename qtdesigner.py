e3b45ec906acdf19187a6b73241860a9b1d4475f
Sebastian Keidler <sebastian.keider@hs-kempten.de>
2020-02-01 14:39:44 +0100

    qtdesigner opens another window

--- qtdesigner.py
+++ qtdesigner.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+1# -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'qtdesigner.ui'
 #
@@ -7,16 +7,36 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from OtherWindow import Ui_OtherWindow
+
+
+
 
 class Ui_MainWindow(object):
+    
+    
+    def openWindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_OtherWindow()
+        self.ui.setupUi(self.window)
+        MainWindow.close()
+        self.window.show()
+            
+    
+    
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(656, 461)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        #self.centralwidget.setCurrentIndex(1) #        
+        
         self.button1 = QtWidgets.QPushButton(self.centralwidget)
         self.button1.setGeometry(QtCore.QRect(200, 210, 231, 61))
         self.button1.setObjectName("button1")
+        self.button1.clicked.connect(self.openWindow)
+        
+        
         self.hsk = QtWidgets.QLabel(self.centralwidget)
         self.hsk.setGeometry(QtCore.QRect(10, 10, 101, 51))
         self.hsk.setText("")
@@ -29,6 +49,10 @@ class Ui_MainWindow(object):
         self.adrive.setPixmap(QtGui.QPixmap("img/adrive.jpg"))
         self.adrive.setScaledContents(True)
         self.adrive.setObjectName("adrive")
+        
+        
+        
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 656, 21))
@@ -55,4 +79,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
